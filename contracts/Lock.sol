faa0391c20178fe17c5a27a72778dba7b0d1a156
xdavinchee <0xdavinchee@gmail.com>
2022-12-28 23:24:03 +0200

    add extra events and bump package versions

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -14,27 +14,55 @@ contract Lock {
     /// @dev The deployer of this smart contract (non-transferable)
     address payable public owner;
 
-    event Withdrawal(uint256 amount, uint256 when);
+    event NewOwner(address indexed oldOwner, address indexed newOwner);
+    event NewUnlockTime(address indexed owner, uint64 unlockTime);
+    event Withdrawal(uint256 amount);
 
     error CannotWithdrawYet();
-    error InvalidInitialUnlockTime();
+    error InvalidUnlockTime();
     error NonOwner();
 
     constructor(uint64 _unlockTime) payable {
-        if (block.timestamp >= _unlockTime) revert InvalidInitialUnlockTime();
+        if (block.timestamp >= _unlockTime) revert InvalidUnlockTime();
 
         unlockTime = _unlockTime;
         owner = payable(msg.sender);
+
+        emit NewUnlockTime(msg.sender, _unlockTime);
+    }
+
+    modifier onlyOwner() {
+        if (msg.sender != owner) revert NonOwner();
+        _;
     }
 
     /// @notice A withdraw function which allows withdrawal
     /// @dev Reverts if non-owner or if still locked.
-    function withdraw() external {
+    function withdraw() external onlyOwner {
         if (block.timestamp < unlockTime) revert CannotWithdrawYet();
-        if (msg.sender != owner) revert NonOwner();
-
-        emit Withdrawal(address(this).balance, block.timestamp);
 
         owner.transfer(address(this).balance);
+
+        emit Withdrawal(address(this).balance);
+    }
+
+    /// @notice Sets the unlock time for the withdrawal
+    /// @dev Explain to a developer any extra details
+    /// @param _unlockTime The time which the lock can be unlocked
+    function setUnlockTime(uint64 _unlockTime) external onlyOwner {
+        if (block.timestamp >= _unlockTime) revert InvalidUnlockTime();
+
+        unlockTime = _unlockTime;
+
+        emit NewUnlockTime(msg.sender, _unlockTime);
+    }
+
+    /// @notice Sets the new owner/admin of the contract
+    /// @dev Allow arbitrarily setting of new owner
+    /// @param _newOwner The new owner of the contract
+    function setNewOwner(address payable _newOwner) external onlyOwner {
+        owner = _newOwner;
+
+        emit NewOwner(msg.sender, _newOwner);
     }
 }
